b0d22894a5c024bf6ef446ac95b73b0a2c83b4d8
hy3098626581 <3098626581@qq.com>
2018-05-11 14:33:12 +0800

    .。

--- app/src/main/java/com/jiyun/yingyuxinyuan/ui/activity/my/store/activity/StoreActivity.java
+++ app/src/main/java/com/jiyun/yingyuxinyuan/ui/activity/my/store/activity/StoreActivity.java
@@ -23,6 +23,9 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ *
+ */
 public class StoreActivity extends AppCompatActivity {
 
     @BindView(R.id.store_cancle)
